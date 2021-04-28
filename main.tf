328bdfb73a35400f348ba47484d43c823c0df585
zm-intern06 <79933310+zm-intern06@users.noreply.github.com>
2021-04-28 08:28:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,35 @@
 #main file
-#n,qsn,sbbbcwx
+provider "azurerm" {
+  features {}
+
+  subscription_id = "25a98a18-5e94-4b21-9d17-e8cf45bfd81f"
+  client_id       = "562821fd-0916-4535-ac31-4025760274ee"
+  client_secret   = "fTNwRHs0~sYHk4hsepV6L.Pmf.NHFyaG-S"
+  tenant_id       = "558506eb-9459-4ef3-b920-ad55c555e729"
+}
+
+terraform {
+  required_providers{
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.56.0"
+    }
+  }
+   backend "remote" {
+    organization = "zm-intern06"
+
+    workspaces {
+      name = "Jenkins-Test"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "test_jenkins"
+  location = "westeurope"
+}
+
+
+
+
+
